775e6d8bb808085904ca673d6d6e3eecad16bf96
Linh Ngo <thlinh.ngo@gmail.com>
2022-11-14 17:45:44 +0800

    Add login UI

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import '../shared/widgets/full_screen_loading_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
+  void _onGoogleSignInButtonPressed() {
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return FullScreenLoadingPage(
-      body: Column(
-        children: [
-
-        ],
+      body: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SignInButton(
+              Buttons.Google,
+              onPressed: _onGoogleSignInButtonPressed,
+            ),
+          ] ,
+        ),
       ),
     );
   }
